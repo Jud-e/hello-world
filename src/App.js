7b64897ed4602c20f505720f429bce3e04632b42
Jude Enuanwa <lenuanwa@gmail.com>
2023-06-28 12:21:14 +0100

    training

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 
 function App() {
+  // const name = "Jude";
   return (
     <div className="App">
-      Hello World
+      <Job salary ={90000} position = "Senior SDE" company = "Amazon"/>
+      <Job salary ={12000} position = "Junior SDE" company = "Google"/>
+      <Job salary ={10000} position = "Project Manager" company = "Netflix"/>
+    </div>
+  );  
+}
+const Job = (props)=>{
+  return(
+    <div>
+      <h1>{props.salary}</h1>
+      <h1>{props.position}</h1>
+      <h1>{props.company}</h1>
     </div>
   );
 }
